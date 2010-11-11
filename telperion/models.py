7ce5ec2c40aa98b6c1927379ae82a2c68b6bb9de
Eric Lubin <elubin@mbp-17.local>
2010-11-10 21:18:35 -0500

    local

--- telperion/models.py
+++ telperion/models.py
@@ -5,13 +5,18 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=100)
     email = models.CharField(max_length=50)
-
+    schools = models.ManyToManyField(School)
 
 class School(models.Model):
-    user = models.ForeignKey(User)
     schoolname = models.CharField(max_length=100)
 
+
 class Essay(models.Model):
-    question = models.CharField(max_length=200)
+    question = models.ForeignKey(Question)
     answer = models.CharField(max_length=5000)
+    user = models.ForeignKey(User)
+
+
+class Question(models.Model):
+    questiontext = models.CharField(max_length=200)
     school = models.ForeignKey(School)
