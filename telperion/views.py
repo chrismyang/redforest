d40e1ad068e9a91ceec481f4272f966fe0402d0c
Eric Lubin <elubin@mbp-17.local>
2010-11-11 16:13:44 -0500

    local

--- telperion/views.py
+++ telperion/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from telperion.models import User
+from django.http import HttpResponse
 
+
+def loginpage(request):
+    return render_to_response('Login.html', {}, context_instance=RequestContext(request))
 
 def login(request):
-    return render_to_response('Login.html', {}, context_instance=RequestContext(request))
+    u = User.objects.get(username=request.POST['username'])
+    if(u.password == request.POST['password']):
+        request.session['uid'] = u.id
+        return HttpResponse("Successfully Logged In")
+    else:
+        return render_to_response('Login.html', {'login': 0}, context_instance=RequestContext(request))
+
