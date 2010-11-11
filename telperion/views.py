e73003ece0bf41e37e57e53b57958cb726320a77
TimothyYang <Robotics@.(none)>
2010-11-10 22:02:27 -0500

    df

--- telperion/views.py
+++ telperion/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.template import Context
+
+def mainpage(request):
+    c = Context({
+        'college_name': college_name,
+        'college_list':["asdf","asdf","fdsa"],
+        'question_list':["question 1","question 2"],
+    })
