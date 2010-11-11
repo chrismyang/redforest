1847ded474fcd7cb9d723196517f9b39cb6bd244
Eric Lubin <elubin@mbp-17.local>
2010-11-10 21:02:20 -0500

    local

--- telperion/models.py
+++ telperion/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+
+
+class School(models.Model):
+    user = models.ForeignKey(User)
+    schoolname = models.CharField(max_length=100)
+
+class Essay(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=5000)
+    school = models.ForeignKey(School)
