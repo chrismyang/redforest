d5090d1ccf1380bab897c2cffa914f51eacb0efb
Eric Lubin <elubin@mbp-17.local>
2010-11-10 20:26:20 -0500

    local

--- settings.py
+++ settings.py
@@ -87,6 +87,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
+
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'redforest.telperion',
